30658247f625fd65d4c5ad976ce2edffade5156e
Thuy Linh <23010633@st.phenikaa-uni.edu.vn>
2025-08-26 09:16:40 +0700

    init

--- bookstore/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ bookstore/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,6 +28,10 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
+        if (Auth::user()->usertype =='admin'){
+            return redirect(route('admin.dashboard'));
+        }
+
         return redirect()->intended(route('dashboard', absolute: false));
     }
 
--- bookstore/database/migrations/2025_08_19_060949_create_addresses_table.php
+++ bookstore/database/migrations/2025_08_19_060949_create_addresses_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('phone');
             $table->string('address');
             $table->boolean('is_default')->default(false);
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
